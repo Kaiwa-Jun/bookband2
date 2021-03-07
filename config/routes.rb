a62fc9d8511e9adb99fb2fa23c1f017475fb450f
kaiwa <kj.11235813213455@gmail.com>
2021-03-07 01:41:04 +0000

    本情報取得

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'reviews/show'
   get 'reviews/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    namespace 'api' do
+    namespace 'api' do #namespace,scope,module違い
     namespace 'v1' do
       get "/search", to: "books#search"
     end
